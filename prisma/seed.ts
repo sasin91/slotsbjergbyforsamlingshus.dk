20837feea619cee272a5004b82f7b10a735c296f
sasin91 <jonas.kerwin.hansen@gmail.com>
2022-06-01 09:24:17 +0200

    yeet notes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -20,31 +20,6 @@ Bestil hos Anne tlf. 2014 4080 - email: <kontakt@slotsbjergbyfesthus.dk>
 *Bon appetit!*
   `.trim(),
     },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-      markdown: `
-  # 90s Mixtape
-  
-  - I wish (Skee-Lo)
-  - This Is How We Do It (Montell Jordan)
-  - Everlong (Foo Fighters)
-  - Ms. Jackson (Outkast)
-  - Interstate Love Song (Stone Temple Pilots)
-  - Killing Me Softly With His Song (Fugees, Ms. Lauryn Hill)
-  - Just a Friend (Biz Markie)
-  - The Man Who Sold The World (Nirvana)
-  - Semi-Charmed Life (Third Eye Blind)
-  - ...Baby One More Time (Britney Spears)
-  - Better Man (Pearl Jam)
-  - It's All Coming Back to Me Now (Céline Dion)
-  - This Kiss (Faith Hill)
-  - Fly Away (Lenny Kravits)
-  - Scar Tissue (Red Hot Chili Peppers)
-  - Santa Monica (Everclear)
-  - C'mon N' Ride it (Quad City DJ's)
-      `.trim(),
-    },
   ];
 
   for (const post of posts) {
@@ -57,16 +32,16 @@ Bestil hos Anne tlf. 2014 4080 - email: <kontakt@slotsbjergbyfesthus.dk>
 }
 
 async function seed() {
-  const email = "rachel@remix.run";
+  const email = "john@example.com";
 
   // cleanup the existing database
   await prisma.user.delete({ where: { email } }).catch(() => {
     // no worries if it doesn't exist yet
   });
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+  const hashedPassword = await bcrypt.hash("secret", 10);
 
-  const user = await prisma.user.create({
+  await prisma.user.create({
     data: {
       email,
       password: {
@@ -74,22 +49,6 @@ async function seed() {
           hash: hashedPassword,
         },
       },
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
     },
   });
 
