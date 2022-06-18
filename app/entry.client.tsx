7bbadd16cdf4d7876c3ae94bd38e244c8b5e4f38
sasin91 <jonas.kerwin.hansen@gmail.com>
2022-06-18 14:16:39 +0200

    global strictmode

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,11 +1,14 @@
 import { RemixBrowser } from "@remix-run/react";
+import { StrictMode } from "react";
 import { hydrate } from "react-dom";
 import { LocaleContextProvider } from "./providers/LocaleProvider";
 const locales = [...window.navigator.languages];
 
 hydrate(
-  <LocaleContextProvider locales={locales}>
-    <RemixBrowser />
-  </LocaleContextProvider>,
+  <StrictMode>
+    <LocaleContextProvider locales={locales}>
+      <RemixBrowser />
+    </LocaleContextProvider>
+  </StrictMode>,
   document
 );
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,6 +3,7 @@ import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
 import { parseAcceptLanguage } from "intl-parse-accept-language";
 import { LocaleContextProvider } from "./providers/LocaleProvider";
+import { StrictMode } from "react";
 
 export default function handleRequest(
   request: Request,
@@ -16,9 +17,11 @@ export default function handleRequest(
   });
 
   const markup = renderToString(
-    <LocaleContextProvider locales={locales}>
-      <RemixServer context={remixContext} url={request.url} />
-    </LocaleContextProvider>
+    <StrictMode>
+      <LocaleContextProvider locales={locales}>
+        <RemixServer context={remixContext} url={request.url} />
+      </LocaleContextProvider>
+    </StrictMode>
   );
 
   responseHeaders.set("Content-Type", "text/html");
